3c8c2284934ee02606aa94f16a352aa6d6d65d26
@Manô_12 <emanuelklismandev001@gmail.com>
2025-04-09 21:28:54 -0300

    adicionando biblioetcas

--- Envia_email.py
+++ Envia_email.py
@@ -1,7 +1,19 @@
 import pyautogui #  Biblioteca para automação
 import time # Bivlioteca para dar pausas no código
+import re # Biblioeteca para expressöes regulares
+
+'''O objetivo do código é bem simples. Consiste em 2 passos, o primeiro é perguntar ao usuário qual email ele deseja enviar, mas antes o código valida o formato do email
+e se for válido envia, caso contrário ele pede um email válido. O segundo passo é inicar a automação do envio de email, ele busca o navegador, cola o link do email 
+envia para o email informado com corpo  e assunto'''
+
 
 email_teste = input("Para qual email voce deseja enviar?") # Solicta ao usuário o email que ele quer enviar
+def valida_email(email): # Valida se o email possui o formato @gmail, se sim ele segue o código
+    padrao = r'^[\w\.-]+@gmail\.com.br$'
+    return re.match(padrao, email) is not None
+if not valida_email(email_teste): # se não ele diga o texto abaixo
+    print("Email inválido! Certifique-se de que é um endereço @gmail.com válido.")
+    exit()  # Encerra o programa se o email for inválido
 pyautogui.PAUSE = 0.5 # pausa 5 sgeundos para em cada linha de código
 pyautogui.press("win") # aperta a tecla 
 pyautogui.write("edge") # Digita o nome do navegador
